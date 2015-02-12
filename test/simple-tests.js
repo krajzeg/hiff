f47802c94b00a1fa9b6460f9a18e92945e23a8d4
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2015-02-12 11:47:36 +0100

    A better battery of tests.

--- test/simple-tests.js
+++ test/simple-tests.js
@@ -2,15 +2,67 @@ var diff = require('../src/hiff').diff;
 var assert = require('chai').assert;
 
 
-describe("diff", function() {
-    it("should report no changes for identical HTML", function() {
-      var html = "<div>Hello!</div>";
-      assert.equal(diff(html, html), false);
-    });
+describe("diff", function () {
+  it("should report no changes for identical HTML", function () {
+    var html = "<div>Hello!</div>";
+    assert.equal(diff(html, html), false);
+  });
 
-    it("should detect tag name changes", function() {
-      var html1 = "<div>Hello!</div>";
-      var html2 = "<b>Hello!</b>";
-      assert(diff(html1, html2));
-    });
+  it("should detect tag name changes", function () {
+    var html1 = "<div>Hello!</div>";
+    var html2 = "<b>Hello!</b>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect changed attributes", function() {
+    var html1 = "<div class='john'></div>";
+    var html2 = "<div class='jack'></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect missing attributes", function() {
+    var html1 = "<div id='J' class='john'></div>";
+    var html2 = "<div class='john'></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect extra attributes", function() {
+    var html1 = "<div class='john'></div>";
+    var html2 = "<div id='J' class='john'></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect text differences", function() {
+    var html1 = "<div class='john'>John</div>";
+    var html2 = "<div class='john'>Jack</div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect missing children", function() {
+    var html1 = "<div class='john'><b>Ja</b><i>ck</i></div>";
+    var html2 = "<div class='john'><b>Ja</b></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect extra children", function() {
+    var html1 = "<div class='john'><b>Ja</b></div>";
+    var html2 = "<div class='john'><b>Ja</b><i>ck</i></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+  it("should detect changed children", function() {
+    var html1 = "<div class='john'><b>Ja</b><i>ck</i></div>";
+    var html2 = "<div class='john'><b>Ja</b><i>smine</i></div>";
+    var difference = diff(html1, html2);
+    assert(difference);
+  });
+
+
 });
