0d34235d3ce3a0bf0d81367e80e30a2f295f2f3c
Jakub Wasilewski <wasyl@wasyl.eu>
2015-02-17 12:49:10 +0100

    Factored prepareOptions() out of diff().

--- src/hiff.js
+++ src/hiff.js
@@ -20,13 +20,8 @@ var DEFAULT_OPTIONS = {
 };
 
 function diff(expected, actual, options) {
-  // sanitize the options object so we don't have to validate everything down the road
-  options = _.defaults(options || {}, DEFAULT_OPTIONS);
-  if (typeof options.ignore == 'string')
-    options.ignore = [options.ignore];
-
-  // make a place to store memoized comparison results
-  options.memo = {};
+  // prepare the options object so we don't have to validate everything down the road
+  options = prepareOptions(options);
 
   // parse both pieces of HTML with cheerio and get the root nodes
   $1 = cheerio.load(expected);
@@ -34,16 +29,28 @@ function diff(expected, actual, options) {
   var $n1 = node($1, $1.root());
   var $n2 = node($2, $2.root());
 
-  // prepare (remove some things that produces false positives)
+  // prepare input (remove or canonicalize things that produce false positives, strip ignored content)
   prepareForDiff($n1, options);
   prepareForDiff($n2, options);
 
   // compare the roots recursively
   var diffObject = compareNodes($n1, $n2, options);
 
-  // strip some of the low-level information that compareNodes uses internally
-  if (diffObject)
-    return diffObject.changes;
-  else
-    return false;
+  // strip some of the low-level information that compareNodes uses internally, return just the list of changes
+  return diffObject && diffObject.changes;
+}
+
+function prepareOptions(options) {
+  // use defaults
+  options = _.defaults(options || {}, DEFAULT_OPTIONS);
+
+  // sanitize some types
+  if (typeof options.ignore == 'string')
+    options.ignore = [options.ignore];
+
+  // make a place to store memoized comparison results
+  options.memo = {};
+
+  // return the new object
+  return options;
 }
