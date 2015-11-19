3f43c4d7401c5cdc9c974cfe84212c5a5348fa0e
Jakub Wasilewski <wasyl@wasyl.eu>
2015-11-19 20:34:58 +0100

    Added test for the new return value from compare().

--- test/simple-tests.js
+++ test/simple-tests.js
@@ -9,6 +9,22 @@ describe("Hiff at the very least", function () {
     assert.notOk(d.different);
   });
 
+  it("should give us access to what was compared in the return value", function() {
+    var html1 = "<div>Hello!</div>";
+    var html2 = "<div>Hi!</div>";
+    var d = compare(html1, html2);
+
+    // check the cheerio'd objects
+    assert.ok(d.$before);
+    assert.equal(d.$before('div').text(), "Hello!");
+    assert.ok(d.$after);
+    assert.equal(d.$after('div').text(), "Hi!");
+
+    // and the HTML string
+    assert.equal(d.before, html1);
+    assert.equal(d.after, html2);
+  });
+
   it("should detect tag name changes", function () {
     var html1 = "<div>Hello!</div>";
     var html2 = "<b>Hello!</b>";
