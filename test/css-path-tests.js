47ac466b44960bbc682dd98d6bd38c98adc46697
Jakub Wasilewski <wasyl@wasyl.eu>
2015-11-19 20:47:03 +0100

    Added a bigger test to verify if CSS paths are generated correctly.

--- test/css-path-tests.js
+++ test/css-path-tests.js
@@ -2,7 +2,8 @@ var compare = require('../src/hiff').compare;
 var cheerio = require('cheerio');
 var nodePath = require('../src/display/node-path');
 var assert = require('chai').assert;
-
+var fs = require('q-io/fs');
+var path = require('path');
 
 describe("nodePath()", function() {
   it("should prefer to identify elements by ID", function() {
@@ -37,6 +38,20 @@ describe("nodePath()", function() {
     var $ = cheerio.load(html);
     var $secondA = $($('a')[1]);
     assert.equal(nodePath($secondA), "a.red:nth-of-type(2)");
+  });
+
+  it("should produce paths that can be fed back to $()", function(done) {
+    fs.read(path.join(__dirname, "inputs/path-roundtrip-test.html")).then(function(html) {
+      var $ = cheerio.load(html);
+
+      // check if CSS paths roundtrip correctly for all nodes
+      $('*').each(function() {
+        var $node = $(this), path = nodePath($node);
+        if ($.html($node) != $.html($(path))) {
+          throw new AssertionError("Path '" + path + "' is incorrect for node:\n" + $.html($node));
+        }
+      });
+    }).then(done).catch(done);
   });
 });
 
